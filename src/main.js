bb0b04c3eb722747d3f81e40fc3d21232324ae96
lemarc <lemarc.dev@gmail.com>
2024-09-03 10:21:20 +0200

    refactor

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 export const connect = async (ctx) => {
   const client = new MongoClient(ctx.env.mongo.url);
-  const db = client.connect();
-  ctx.dbs.mongo = {
-    client,
-    db,
-  };
+  const { logger } = ctx;
+  try {
+    await client.connect();
+    await client.db().admin().ping();
+
+    logger.info("Successfully connected to MongoDB");
+
+    const db = client.db();
+
+    ctx.dbs.mongo = {
+      client,
+      db,
+    };
+  } catch (error) {
+    logger.error("Failed to connect to MongoDB:", error);
+    throw error;
+  }
 };
 
 export const disconnect = async (ctx) => {
-  ctx.dbs.client.close();
+  const { logger } = ctx;
+  if (ctx.dbs.mongo && ctx.dbs.mongo.client) {
+    await ctx.dbs.mongo.client.close();
+    logger.info("Disconnected from MongoDB");
+  }
 };
